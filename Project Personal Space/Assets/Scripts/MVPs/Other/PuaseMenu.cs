277af8f0cd25982da2c9cac032b016d1fe38f6ef
YingSeh <lecunnane@gmail.com>
2018-06-01 19:08:19 +1000

    pause script written

---
+++ Project Personal Space/Assets/Scripts/MVPs/Other/PuaseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuaseMenu : MonoBehaviour {
+	public static bool TimeOut = false;
+	public GameObject PauseUI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			if (TimeOut) 
+			{
+				Resume ();
+			} 
+			else
+			{
+				Pause ();
+			}
+		}	
+	}
+	void Resume()
+	{
+		PauseUI.SetActive (false);
+		Time.timeScale = 1f;
+		TimeOut = false;
+	}
+	void Pause ()
+	{
+		PauseUI.SetActive (true);
+		Time.timeScale = 0f;
+		TimeOut = true;
+	}
+}
